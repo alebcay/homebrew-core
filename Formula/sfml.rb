17652da01a45a4cadc79426d815e3556f987b17c
Caleb Xu <calebcenter@live.com>
2019-02-22 22:44:41 -0500

    sfml 2.5.1

--- Formula/sfml.rb
+++ Formula/sfml.rb
@@ -2,9 +2,8 @@ class Sfml < Formula
   # Don't update SFML until there's a corresponding CSFML release
   desc "Multi-media library with bindings for multiple languages"
   homepage "https://www.sfml-dev.org/"
-  url "https://www.sfml-dev.org/files/SFML-2.4.2-sources.zip"
-  sha256 "8ba04f6fde6a7b42527d69742c49da2ac529354f71f553409f9f821d618de4b6"
-  revision 1
+  url "https://www.sfml-dev.org/files/SFML-2.5.1-sources.zip"
+  sha256 "bf1e0643acb92369b24572b703473af60bac82caf5af61e77c063b779471bb7f"
   head "https://github.com/SFML/SFML.git"
 
   bottle do
