5840fa55c227b8a3dc0fa3dd280c2677f18dd341
Caleb Xu <calebcenter@live.com>
2019-02-25 12:47:26 -0500

    civl 1.19-5139

--- Formula/civl.rb
+++ Formula/civl.rb
@@ -1,10 +1,9 @@
 class Civl < Formula
   desc "The Concurrency Intermediate Verification Language"
   homepage "https://vsl.cis.udel.edu/civl/"
-  url "https://vsl.cis.udel.edu/lib/sw/civl/1.7/r3157/release/CIVL-1.7_3157.tgz"
-  version "1.7-3157"
-  sha256 "49ed0467ea281bf5a436b2caf4f87862d3f613fa9e6e746ce52cfd409c3f4403"
-  revision 1
+  url "https://vsl.cis.udel.edu/lib/sw/civl/1.19/r5139/release/CIVL-1.19_5139.tgz"
+  version "1.19-5139"
+  sha256 "1a372b68710af202daaf7bdeb2441ece3c87bc905cae0ed57f5c0585105cbb3e"
 
   bottle :unneeded
 
@@ -12,8 +11,8 @@ class Civl < Formula
   depends_on "z3"
 
   def install
-    libexec.install "lib/civl-1.7_3157.jar"
-    bin.write_jar_script libexec/"civl-1.7_3157.jar", "civl"
+    libexec.install "lib/civl-1.19_5139.jar"
+    bin.write_jar_script libexec/"civl-1.19_5139.jar", "civl"
     pkgshare.install "doc", "emacs", "examples", "licenses"
   end
 
