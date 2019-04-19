35f2c4b6603e5bd37b4c603ddf552312b3be4a22
Caleb Xu <calebcenter@live.com>
2019-04-19 00:31:17 -0400

    dsd: don't build with mbelib

--- Formula/dsd.rb
+++ Formula/dsd.rb
@@ -6,6 +6,7 @@ class Dsd < Formula
   stable do
     url "https://github.com/szechyjs/dsd/archive/v1.6.0.tar.gz"
     sha256 "44fa3ae108d2c11b4b67388d37fc6a63e8b44fc72fdd5db7b57d9eb045a9df58"
+    revision 1
 
     patch do
       # Fixes build on macOS.
@@ -29,26 +30,7 @@ class Dsd < Formula
   depends_on "libsndfile"
   depends_on "portaudio"
 
-  resource "mbelib-1.2.5" do
-    url "https://github.com/szechyjs/mbelib/archive/v1.2.5.tar.gz"
-    sha256 "59d5e821b976a57f1eae84dd57ba84fd980d068369de0bc6a75c92f0b286c504"
-  end
-
   def install
-    resource("mbelib-1.2.5").stage do
-      # only want the static library
-      inreplace "CMakeLists.txt",
-        "install(TARGETS mbe-static mbe-shared DESTINATION lib)",
-        "install(TARGETS mbe-static DESTINATION lib)"
-      args = std_cmake_args
-      args << "-DCMAKE_INSTALL_PREFIX=#{buildpath}/vendor/mbelib"
-      system "cmake", ".", *args
-      system "make", "install"
-    end
-
-    ENV.prepend "LDFLAGS", "-L#{buildpath}/vendor/mbelib/lib -lmbe"
-    buildpath.install_symlink buildpath/"vendor/mbelib/include/mbelib.h"
-
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
