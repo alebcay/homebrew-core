3c470e1147eab8eed718ab513945d974551fdb3a
Caleb Xu <calebcenter@live.com>
2020-05-12 10:39:18 -0400

    vtk 9.0.0

--- Formula/vtk.rb
+++ Formula/vtk.rb
@@ -1,25 +1,9 @@
 class Vtk < Formula
   desc "Toolkit for 3D computer graphics, image processing, and visualization"
   homepage "https://www.vtk.org/"
-  revision 10
+  url "https://www.vtk.org/files/release/9.0/VTK-9.0.0.tar.gz"
+  sha256 "15def4e6f84d72f82386617fe595ec124dda3cbd13ea19a0dcd91583197d8715"
   head "https://github.com/Kitware/VTK.git"
-
-  stable do
-    url "https://www.vtk.org/files/release/8.2/VTK-8.2.0.tar.gz"
-    sha256 "34c3dc775261be5e45a8049155f7228b6bd668106c72a3c435d95730d17d57bb"
-
-    # Fix compile issues on Mojave and later
-    patch do
-      url "https://gitlab.kitware.com/vtk/vtk/commit/ca3b5a50d945b6e65f0e764b3138cad17bd7eb8d.diff"
-      sha256 "b9f7a3ebf3c29f3cad4327eb15844ac0ee849755b148b60fef006314de8e822e"
-    end
-
-    # Python 3.8 compatibility
-    patch do
-      url "https://gitlab.kitware.com/vtk/vtk/commit/257b9d7b18d5f3db3fe099dc18f230e23f7dfbab.diff"
-      sha256 "572c06a4ba279a133bfdcf0190fec2eff5f330fa85ad6a2a0b0f6dfdea01ca69"
-    end
-  end
 
   bottle do
     sha256 "37a13865cbf0c68db548b85f9577242b95a14c148db1c81548d82aac578fbf25" => :catalina
@@ -75,10 +59,7 @@ class Vtk < Formula
       system "make", "install"
     end
 
-    # Avoid hard-coding HDF5's Cellar path
-    inreplace Dir["#{lib}/cmake/**/vtkhdf5.cmake"].first,
-              Formula["hdf5"].prefix.realpath,
-              Formula["hdf5"].opt_prefix
+    inreplace include/"vtk-9.0/vtkConfigure.h", HOMEBREW_LIBRARY/"Homebrew/shims/mac/super/clang++", "/usr/bin/clang"
   end
 
   test do
