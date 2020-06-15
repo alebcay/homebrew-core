15edc81b9e79a30c89efa5bfe496862621e2c182
Caleb Xu <calebcenter@live.com>
2020-06-15 09:12:45 -0400

    aws-sdk-cpp 1.7.360

--- Formula/aws-sdk-cpp.rb
+++ Formula/aws-sdk-cpp.rb
@@ -2,8 +2,8 @@ class AwsSdkCpp < Formula
   desc "AWS SDK for C++"
   homepage "https://github.com/aws/aws-sdk-cpp"
   # aws-sdk-cpp should only be updated every 10 releases on multiples of 10
-  url "https://github.com/aws/aws-sdk-cpp/archive/1.7.340.tar.gz"
-  sha256 "2e82517045efb55409cff1408c12829d9e8aea22c1e2888529cb769b7473b0bf"
+  url "https://github.com/aws/aws-sdk-cpp/archive/1.7.360.tar.gz"
+  sha256 "f0b6fd26e8d0cbc2df7a7ccc435fc85680abe15ddbb0ceff67f5c674c8fe6550"
   head "https://github.com/aws/aws-sdk-cpp.git"
 
   bottle do
