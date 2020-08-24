cf3f67028a9a56f550500f0e21090230906336c5
Caleb Xu <calebcenter@live.com>
2020-08-24 18:35:24 -0400

    vault-cli: allow JAVA_HOME override

--- Formula/vault-cli.rb
+++ Formula/vault-cli.rb
@@ -4,6 +4,7 @@ class VaultCli < Formula
   url "https://search.maven.org/remotecontent?filepath=org/apache/jackrabbit/vault/vault-cli/3.4.6/vault-cli-3.4.6-bin.tar.gz"
   sha256 "0a5a6fcb0dd40abe716dada3d86f1123b780e62324a9452af6718b1c989a3325"
   license "Apache-2.0"
+  revision 1
   head "https://github.com/apache/jackrabbit-filevault.git"
 
   bottle :unneeded
@@ -16,7 +17,7 @@ class VaultCli < Formula
 
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix)
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
   end
 
   test do
