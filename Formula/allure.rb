03e6248257786e29f1c1c0a9e656856d962fc5bd
Caleb Xu <calebcenter@live.com>
2020-08-24 18:35:21 -0400

    allure: allow JAVA_HOME override

--- Formula/allure.rb
+++ Formula/allure.rb
@@ -4,6 +4,7 @@ class Allure < Formula
   url "https://dl.bintray.com/qameta/maven/io/qameta/allure/allure-commandline/2.13.5/allure-commandline-2.13.5.zip"
   sha256 "60f9ca89c5523c9431c13f5a22165fe67164e2f484f0718f993de9ab87896cd0"
   license "Apache-2.0"
+  revision 1
 
   bottle :unneeded
 
@@ -16,7 +17,7 @@ class Allure < Formula
     prefix.install_metafiles
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
   end
 
   test do
