f91724f3ed83084f6bce0b5ccbf19ace9d8eb369
Caleb Xu <calebcenter@live.com>
2020-08-24 18:35:23 -0400

    mallet: allow JAVA_HOME override

--- Formula/mallet.rb
+++ Formula/mallet.rb
@@ -3,7 +3,8 @@ class Mallet < Formula
   homepage "http://mallet.cs.umass.edu/"
   url "http://mallet.cs.umass.edu/dist/mallet-2.0.8.tar.gz"
   sha256 "5b2d6fb9bcf600b1836b09881821a6781dd45a7d3032e61d7500d027a5b34faf"
-  revision 1
+  license "CPL-1.0"
+  revision 2
 
   bottle :unneeded
 
@@ -18,7 +19,7 @@ class Mallet < Formula
     rm Dir["bin/*.{bat,dll,exe}"] # Remove all windows files
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/bin/*"]
-    bin.env_script_all_files(libexec/"bin", JAVA_HOME: Formula["openjdk"].opt_prefix)
+    bin.env_script_all_files libexec/"bin", Language::Java.overridable_java_home_env
   end
 
   test do
