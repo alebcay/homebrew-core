88cc3103b58fb61f329812ef00671236e4e6bbd0
Caleb Xu <calebcenter@live.com>
2020-08-24 18:35:24 -0400

    stanford-parser: allow JAVA_HOME override

--- Formula/stanford-parser.rb
+++ Formula/stanford-parser.rb
@@ -4,7 +4,8 @@ class StanfordParser < Formula
   url "https://nlp.stanford.edu/software/stanford-parser-full-2018-10-17.zip"
   version "3.9.2"
   sha256 "92d852af54c0727c2367b9ce267c53bf08f1551a08ec5dd92c357b8cc7b2bcd9"
-  revision 1
+  license "GPL-2.0-or-later"
+  revision 2
 
   bottle :unneeded
 
@@ -13,7 +14,7 @@ class StanfordParser < Formula
   def install
     libexec.install Dir["*"]
     bin.install Dir["#{libexec}/*.sh"]
-    bin.env_script_all_files libexec, JAVA_HOME: Formula["openjdk"].opt_prefix
+    bin.env_script_all_files libexec, Language::Java.overridable_java_home_env
   end
 
   test do
