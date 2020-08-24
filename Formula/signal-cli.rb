b0792b0b9f278a27f886211d24d90ebd1238ff74
Caleb Xu <calebcenter@live.com>
2020-08-24 18:35:23 -0400

    signal-cli: allow JAVA_HOME override

--- Formula/signal-cli.rb
+++ Formula/signal-cli.rb
@@ -3,7 +3,8 @@ class SignalCli < Formula
   homepage "https://github.com/AsamK/signal-cli"
   url "https://github.com/AsamK/signal-cli/releases/download/v0.6.8/signal-cli-0.6.8.tar.gz"
   sha256 "6db702aa940cc9ddaa5952ef24f6c18acdc12b704b59f77fb1df947032929e3d"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
+  revision 1
 
   bottle :unneeded
 
@@ -11,7 +12,7 @@ class SignalCli < Formula
 
   def install
     libexec.install Dir["lib", "bin"]
-    (bin/"signal-cli").write_env_script libexec/"bin/signal-cli", JAVA_HOME: Formula["openjdk"].opt_prefix
+    (bin/"signal-cli").write_env_script libexec/"bin/signal-cli", Language::Java.overridable_java_home_env
   end
 
   test do
