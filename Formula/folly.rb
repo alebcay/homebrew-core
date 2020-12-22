1dbf4d6c01107726a5e079683cd21dd69db71114
Caleb Xu <calebcenter@live.com>
2020-12-22 10:44:00 -0500

    folly 2020.12.21.00

--- Formula/folly.rb
+++ Formula/folly.rb
@@ -1,8 +1,8 @@
 class Folly < Formula
   desc "Collection of reusable C++ library artifacts developed at Facebook"
   homepage "https://github.com/facebook/folly"
-  url "https://github.com/facebook/folly/archive/v2020.12.14.00.tar.gz"
-  sha256 "a3590caac887575bc0a936ae2380126c0edf754c9bdaef7ed84316f68665c791"
+  url "https://github.com/facebook/folly/archive/v2020.12.21.00.tar.gz"
+  sha256 "db2b8622df68b09e1323116da3e7635c58e3ad05f983f58c07aa2290a9c6b88e"
   license "Apache-2.0"
   head "https://github.com/facebook/folly.git"
 
