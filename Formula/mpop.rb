671c7c3548fc374c46f5b19a8949c16a6fc3a41e
Caleb Xu <calebcenter@live.com>
2020-12-24 23:41:10 -0500

    mpop 1.4.12

--- Formula/mpop.rb
+++ Formula/mpop.rb
@@ -1,8 +1,8 @@
 class Mpop < Formula
   desc "POP3 client"
   homepage "https://marlam.de/mpop/"
-  url "https://marlam.de/mpop/releases/mpop-1.4.11.tar.xz"
-  sha256 "9b53df519d47b5e709aa066b54b97f0cdca84a06c92dbcf52118f86223dc9dbd"
+  url "https://marlam.de/mpop/releases/mpop-1.4.12.tar.xz"
+  sha256 "5f6355b52d9c360619623a40c66c1a5571e393b43fe58375c0de35429ac3480a"
   license "GPL-3.0"
 
   bottle do
