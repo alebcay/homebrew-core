84994b1a3f1adcf6c855466722364c588c7803de
Caleb Xu <calebcenter@live.com>
2021-07-21 21:16:06 -0400

    biosig 2.3.1

--- Formula/biosig.rb
+++ Formula/biosig.rb
@@ -1,8 +1,8 @@
 class Biosig < Formula
   desc "Tools for biomedical signal processing and data conversion"
   homepage "https://biosig.sourceforge.io"
-  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.3.0.src.tar.gz"
-  sha256 "9c66d1167628d6c2027168fb82bcd3c28357e59bd5d8b1d9e615457a2eb59ca0"
+  url "https://downloads.sourceforge.net/project/biosig/BioSig%20for%20C_C%2B%2B/src/biosig-2.3.1.src.tar.gz"
+  sha256 "1edf6228ac99456d551ba88c997a493d39fc9ca09f4442e27e1470a30c3a34b9"
   license "GPL-3.0-or-later"
 
   livecheck do
