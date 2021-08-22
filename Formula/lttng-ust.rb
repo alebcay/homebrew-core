92cf7f6e2164dc4d6580a538258ecf620bc565fb
Caleb Xu <calebcenter@live.com>
2021-08-22 01:31:22 -0400

    lttng-ust 2.13.0

--- Formula/lttng-ust.rb
+++ Formula/lttng-ust.rb
@@ -1,14 +1,15 @@
 class LttngUst < Formula
   desc "Linux Trace Toolkit Next Generation Userspace Tracer"
   homepage "https://lttng.org/"
-  url "https://lttng.org/files/lttng-ust/lttng-ust-2.12.2.tar.bz2"
-  sha256 "bcd0f064b6ca88c72d84e760eac3472ae5c828411c634435922bee9fce359fc7"
+  url "https://lttng.org/files/lttng-ust/lttng-ust-2.13.0.tar.bz2"
+  sha256 "54e4c933679cf6a07971dc5861ce57fc4876ab740ab612407b30b5fc85371750"
   license all_of: ["LGPL-2.1-only", "MIT", "GPL-2.0-only", "BSD-3-Clause", "BSD-2-Clause", "GPL-3.0-or-later"]
 
   bottle do
     sha256 cellar: :any_skip_relocation, x86_64_linux: "2a1aaa6c23382092d5f4e8422dd1db1330b46a80631c63bb4f3441d04dc30244"
   end
 
+  depends_on "pkg-config" => :build
   depends_on :linux
   depends_on "numactl"
   depends_on "userspace-rcu"
