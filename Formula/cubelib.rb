203163c80e04f951b3566bd4f57fdfdb520776ee
Caleb Xu <calebcenter@live.com>
2022-01-24 09:28:51 -0500

    cubelib: fix flat_namespace usage

--- Formula/cubelib.rb
+++ Formula/cubelib.rb
@@ -1,6 +1,6 @@
 class Cubelib < Formula
   desc "Cube, is a performance report explorer for Scalasca and Score-P"
-  homepage "https://scalasca.org/software/cube-4.x/download.html"
+  homepage "https://scalasca.org/scalesca/software/cube-4.x/download.html"
   url "https://apps.fz-juelich.de/scalasca/releases/cube/4.6/dist/cubelib-4.6.tar.gz"
   sha256 "36eaffa7688db8b9304c9e48ca5dc4edc2cb66538aaf48657b9b5ccd7979385b"
 
@@ -16,8 +16,30 @@ class Cubelib < Formula
     sha256 mojave:        "5eca16fc5e707d28e6595f070a49516d65a46aa495b95ab78616777027e36114"
   end
 
+  # Fix -flat_namespace being used on Big Sur and later.
+  patch :DATA
+
+  depends_on "autoconf" => :build
+  depends_on "autoconf-archive" => :build
+  depends_on "automake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libtool" => :build
+
   def install
     ENV.deparallelize
+
+    inreplace "build-frontend/configure.ac",
+              "[AC_MSG_ERROR([No ISO C99 support in C compiler.])]",
+              "AC_MSG_ERROR([No ISO C99 support in C compiler.])"
+
+    inreplace "build-config/common/m4/ac_scorep_c99.m4" do |s|
+      s.gsub! "[_AC_C_STD_TRY([c99],", "_AC_C_STD_TRY([c99],"
+      s.gsub! "])# _AC_SCOREP_PROG_CC_C99", ")# _AC_SCOREP_PROG_CC_C99"
+    end
+
+    cd "build-frontend" do
+      system "autoreconf", "-ivf"
+    end
 
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
@@ -39,3 +61,50 @@ class Cubelib < Formula
     end
   end
 end
+__END__
+--- a/build-frontend/configure.orig	2021-10-01 08:15:08.000000000 -0700
++++ b/build-frontend/configure		  2021-10-20 12:44:47.000000000 -0700
+@@ -8733,16 +8733,11 @@
+       _lt_dar_allow_undefined='$wl-undefined ${wl}suppress' ;;
+     darwin1.*)
+       _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-    darwin*) # darwin 5.x on
+-      # if running on 10.5 or later, the deployment target defaults
+-      # to the OS version, if on x86, and 10.4, the deployment
+-      # target defaults to 10.4. Don't you love it?
+-      case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+-	10.0,*86*-darwin8*|10.0,*-darwin[91]*)
+-	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+-	10.[012][,.]*)
++    darwin*)
++      case ${MACOSX_DEPLOYMENT_TARGET},$host in
++	10.[012],*|,*powerpc*)
+ 	  _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-	10.*)
++	*)
+ 	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+       esac
+     ;;
+--- a/build-config/m4/libtool.m4.orig	2021-10-01 08:15:08.000000000 -0700
++++ b/build-config/m4/libtool.m4		  2021-10-20 12:44:47.000000000 -0700
+@@ -1067,16 +1067,11 @@ _LT_EOF
+       _lt_dar_allow_undefined='$wl-undefined ${wl}suppress' ;;
+     darwin1.*)
+       _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-    darwin*) # darwin 5.x on
+-      # if running on 10.5 or later, the deployment target defaults
+-      # to the OS version, if on x86, and 10.4, the deployment
+-      # target defaults to 10.4. Don't you love it?
+-      case ${MACOSX_DEPLOYMENT_TARGET-10.0},$host in
+-	10.0,*86*-darwin8*|10.0,*-darwin[[91]]*)
+-	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+-	10.[[012]][[,.]]*)
++    darwin*)
++      case ${MACOSX_DEPLOYMENT_TARGET},$host in
++	10.[[012]],*|,*powerpc*)
+ 	  _lt_dar_allow_undefined='$wl-flat_namespace $wl-undefined ${wl}suppress' ;;
+-	10.*)
++	*)
+ 	  _lt_dar_allow_undefined='$wl-undefined ${wl}dynamic_lookup' ;;
+       esac
+     ;;
