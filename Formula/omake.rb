36639c25141cba02d5adf2f3f051bc2d799ca3cd
Caleb Xu <calebcenter@live.com>
2022-06-08 00:18:59 -0400

    omake 0.10.4

--- Formula/omake.rb
+++ Formula/omake.rb
@@ -1,8 +1,8 @@
 class Omake < Formula
   desc "Build system designed for scalability, portability, and concision"
   homepage "http://projects.camlcity.org/projects/omake.html"
-  url "https://github.com/ocaml-omake/omake/archive/omake-0.10.3.tar.gz"
-  sha256 "5f42aabdb4088b5c4e86c7a08e235dc7d537fd6b3064852154303bb92f5df70e"
+  url "https://github.com/ocaml-omake/omake/archive/omake-0.10.4.tar.gz"
+  sha256 "0911299173e9e98326d7a09fc26b226f5e5589a5cd5ba5e38c31fe5cd05fd698"
   license "GPL-2.0-only"
   head "https://github.com/ocaml-omake/omake.git", branch: "master"
 
