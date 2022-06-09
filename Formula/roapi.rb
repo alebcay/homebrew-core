df58f51dcc6ff3f5b3b0602d36695b5d8e1d2e97
Caleb Xu <calebcenter@live.com>
2022-06-09 11:19:14 -0400

    roapi 0.7.1

--- Formula/roapi.rb
+++ Formula/roapi.rb
@@ -1,8 +1,8 @@
 class Roapi < Formula
   desc "Full-fledged APIs for static datasets without writing a single line of code"
   homepage "https://roapi.github.io/docs"
-  url "https://github.com/roapi/roapi/archive/refs/tags/roapi-http-v0.6.0.tar.gz"
-  sha256 "f34442a5a4531fd5e0694ed5c62ddf7e1795d940cabc7346bc7a8cac3af73923"
+  url "https://github.com/roapi/roapi/archive/refs/tags/roapi-v0.7.1.tar.gz"
+  sha256 "5abe5e4d9c58f6b5be951c8889e98f718c329c0fd5030825965217b6abe98df1"
   license "Apache-2.0"
 
   bottle do
